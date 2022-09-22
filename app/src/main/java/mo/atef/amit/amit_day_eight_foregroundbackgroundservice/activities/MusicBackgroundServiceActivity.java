bd5decd2d96c7b2c07dc8488352e76d96df66fe0
Mohamed Atef <proofesser@gmail.com>
2022-09-22 18:46:55 +0200

    BackgroundService implementation

---
+++ app/src/main/java/mo/atef/amit/amit_day_eight_foregroundbackgroundservice/activities/MusicBackgroundServiceActivity.java
@@ -0,0 +1,50 @@
+package mo.atef.amit.amit_day_eight_foregroundbackgroundservice.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import mo.atef.amit.amit_day_eight_foregroundbackgroundservice.R;
+import mo.atef.amit.amit_day_eight_foregroundbackgroundservice.databinding.ActivityBackgroundServiceBinding;
+import mo.atef.amit.amit_day_eight_foregroundbackgroundservice.service.BackgroundService;
+
+public class MusicBackgroundServiceActivity extends AppCompatActivity {
+
+    ActivityBackgroundServiceBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background_service);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_background_service);
+        binding.btnLaunchMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isMyServiceRunning(MusicBackgroundServiceActivity.class)){
+                    //true
+                    binding.btnLaunchMusic.setText("Stopped");
+                    stopService(new Intent(MusicBackgroundServiceActivity.this, BackgroundService.class));
+                }else {
+                    //false
+                    binding.btnLaunchMusic.setText("Started");
+                    startService(new Intent(MusicBackgroundServiceActivity.this, BackgroundService.class));
+                }
+            }
+        });
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/mo/atef/amit/amit_day_eight_foregroundbackgroundservice/service/BackgroundService.java
@@ -0,0 +1,47 @@
+package mo.atef.amit.amit_day_eight_foregroundbackgroundservice.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import mo.atef.amit.amit_day_eight_foregroundbackgroundservice.R;
+
+public class BackgroundService extends Service {
+    private MediaPlayer musicPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        musicPlayer = MediaPlayer.create(this, R.raw.amr);
+        musicPlayer.setLooping(false);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Music Service started by user.", Toast.LENGTH_LONG).show();
+        musicPlayer.start();
+        Log.e("Background","Started");
+        return START_STICKY;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        musicPlayer.stop();
+        Log.e("Background","Destroyed");
+        Toast.makeText(this, "Music Service destroyed by user.", Toast.LENGTH_LONG).show();
+    }
+}
